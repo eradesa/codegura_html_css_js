bfd643d83e085b4a357ccb3f983fbfad0db3e30f
eradesa <eranga.de.saram@gmail.com>
2025-06-07 14:40:52 +0530

    test

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ console.log('test2');
 
 myName="Eranga"
 console.log(myName)
+//alert(myName)
+num01 = 5
+num02 = 4
+
+total = num01 + num02
+console.log(total)
+document.getElementById("display01").textContent=total
+
+
+
